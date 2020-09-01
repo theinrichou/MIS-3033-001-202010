86c898de83cc3c23f8bf78d59d34b22c479eb28c
Owner <Owner@DESKTOP-DHLQ5SC>
2020-09-01 18:02:56 -0500

    Did some cool stuff!

---
+++ ReviewProblems/ClassesAndFiles/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesAndFiles
+{
+    class Class1
+    {
+        public string name { get; set; }
+
+        public int id {get;}
+
+        public DateTime enrollDate { get; set; }
+
+        public static int totalStudents { get; set; }
+
+        public Class1 (string name, int id)
+        {
+            this.name = name;
+            this.id = id;
+            enrollDate = DateTime.Now;
+
+        }
+
+        public Class1()
+        {
+            name = string.Empty;
+            id = -1;
+            enrollDate = DateTime.Now;
+        }
+
+        public string getName ()
+        {
+            return name;
+        }
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+
+
+    }
+}
--- ReviewProblems/ClassesAndFiles/Program.cs
+++ ReviewProblems/ClassesAndFiles/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ClassesAndFiles
 {
@@ -6,7 +7,27 @@ namespace ClassesAndFiles
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Class1> students = new List<Class1>;
+          
+            Class1 stud1 = new Class1();
+            Class1.name = "Micah";
+            Class1.id = 2;
+            students.Add(stud1);
+
+            Class1 stud2 = new Class1("Adam", 1);
+            students.Add(stud2);
+
+            foreach (var stud in students)
+            {
+
+                Console.WriteLine($"{stud.name} has an id of {stud.id}");
+            }
+
+
+
+            //Console.WriteLine($"{stud1.name} has an id of { stud1.id}");
+            //Console.WriteLine($"{stud2.name} has an id of { stud2.id}");
+
         }
     }
 }
